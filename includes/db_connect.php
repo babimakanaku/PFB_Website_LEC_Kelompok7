57d0059c5029fc3ec458e40c5912c19ff1be9081
Nico <nicolamarvels@gmail.com>
2025-12-22 20:55:24 +0700

    Initial commit of the Tukar Buku project structure

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,16 +1,11 @@
 <?php
-// Konfigurasi Database
 $servername = "localhost";
-$username = "root"; // Ganti dengan username DB Anda
-$password = "";     // Ganti dengan password DB Anda
-$dbname = "db_tukar_buku"; // Ganti dengan nama DB Anda
+$username = "root";
+$password = "";
+$dbname = "db_tukar_buku";
 
-// Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Cek koneksi
 if ($conn->connect_error) {
     die("Koneksi Gagal: " . $conn->connect_error);
 }
-// echo "Koneksi Berhasil"; // Hapus baris ini setelah berhasil
 ?>
